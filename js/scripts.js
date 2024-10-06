dd22808148d965dfb6d8f0f56e432891e896fb1d
Adrianthebeloved <anthonyoliko@gmail.com>
2024-10-06 19:15:42 +0100

    added

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function () {
+    var now = new Date();
+    var day = now.getDate();
+    var month = now.getMonth();
+    var year = now.getFullYear() + 1;
+
+    var nextyear = month + '/' + day + '/' + year + ' 07:07:07';
+
+    $('#timer').countdown({
+        // date: nextyear, //TODO Date format: 07/27/2017 17:00:00
+        date: '10/29/2023 17:00:00', //TODO Date format: 07/27/2017 17:00:00
+        offset: +2,
+        day: 'Day',
+        days: 'Days'
+    }, function () {
+    });
+});
